b8a94e2d58de553170b6513b2dd7d185202255a8
handasat <hndsttknh@gmail.com>
2026-01-19 22:26:18 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDZVrncvPwEg6IDHjloweJNjrpj32lZy5g",
+  authDomain: "zygo-acf32.firebaseapp.com",
+  databaseURL: "https://zygo-acf32-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "zygo-acf32",
+  storageBucket: "zygo-acf32.firebasestorage.app",
+  messagingSenderId: "243652763597",
+  appId: "1:243652763597:web:f9cbc63435193855236e84",
+  measurementId: "G-NLS5ZK08JZ"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
