d78b15940db336faf23f5d4f9db224de8b2d594c
handasat <hndsttknh@gmail.com>
2026-01-21 10:49:44 +0200

    Update buyTicket.js

--- functions/automation/buyTicket.js
+++ functions/automation/buyTicket.js
@@ -1,1 +1,9 @@
+async function buyTicket(userData) {
+  console.log("🤖 Automation started");
+  console.log("Buying ticket for:", userData.email);
+
+  // כאן תבוא האוטומציה בהמשך
+}
+
+module.exports = { buyTicket };
 
